0c9de175a0d0985dc362086dae1136a9eb3685aa
Sarida Pinyoying <jrysrd@gmail.com>
2024-02-22 21:04:27 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :rooms, except: %i[edit destroy] do
+    resources :appointments, only: :create
+  end
+  resources :users, only: :show do
+    resources :appointments, only: %i[index update]
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
