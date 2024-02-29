244164fb4254557f4db6a3bbf3d47ff4fb121543
Julien Laloye <julienlaloye@outlook.fr>
2024-02-29 21:06:05 +0100

    Adding Seeds, correcting validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,14 +4,20 @@ class Room < ApplicationRecord
 
   gender_options = ["men", "women", "no preference"]
   validates :title, :description, :gender, :date, :max_part, :min_part, :language, presence: true
-  validates :description, length: { in: (50...400) }
+  # validates :description, length: { in: (50...400) }
   validates :min_age, numericality: { greater_than_or_equal_to: 18, message: "must be at least 18" }
   validates :max_age, numericality: { less_than: 99, message: "must be bellow 99" }
+  # max age must be > min age
+  validates_comparison_of :max_age, greater_than: :min_age
   validates :gender, inclusion: { in: gender_options }
-  validate :date_must_be_in_the_future
+  # validate :date_must_be_in_the_future
   validates :min_part, numericality: { greater_than_or_equal_to: 2, message: "must be at least 2" }
-  validates :max_part, numericality: { less_than: 10, message: "must be bellow 10 " }
-
+  # validates :max_part, numericality: { less_than: 10, message: "must be bellow 10 " }
+  # max_part must be > min part
+  validates_comparison_of :max_part, greater_or_equal_than: :min_part
+  # participants < max_part
+  validates_comparison_of :max_part, greater_or_equal_than: :participants
+  
   private
 
   def date_must_be_in_the_future
