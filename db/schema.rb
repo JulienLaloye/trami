e84a204febbad02a0d5f31f0feeaec5cc5a5b9ff
Julien Laloye <julienlaloye@outlook.fr>
2024-03-02 17:47:50 +0100

    Adding Users ranking update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_02_133332) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_162324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_133332) do
     t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
   create_table "rankings", force: :cascade do |t|
@@ -60,6 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_133332) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "level"
   end
 
   create_table "reports", force: :cascade do |t|
