8722c1e9004d261e0fc185511239d4b8a128322b
Julien Laloye <julienlaloye@outlook.fr>
2024-03-02 11:26:38 +0100

    Fixing the models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,8 @@
 class Appointment < ApplicationRecord
   has_many :users
+  has_many :rooms
   belongs_to :room
+  belongs_to :user
   validates :ownership, presence: true
   # validate uniqueness of the couple room user
 end
