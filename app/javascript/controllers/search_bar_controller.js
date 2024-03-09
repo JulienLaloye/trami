7ad4fbab99fd00e566b560878aa1e0d126b44f9e
bhawana srivastava <bhawana.sri2021@gmail.com>
2024-03-09 18:04:10 +0100

    roomsindexpage filter check

--- app/javascript/controllers/search_bar_controller.js
+++ app/javascript/controllers/search_bar_controller.js
@@ -6,11 +6,10 @@ export default class extends Controller {
   connect() {
     console.log("Hello, Stimulus!", this.element)
   }
-
   search(event) {
     event.preventDefault()
     console.log("searching for")
     const currentMood = this.selectMoodTargets
-    currentMood === "social"
+    currentMood === "add"
   }
 }
