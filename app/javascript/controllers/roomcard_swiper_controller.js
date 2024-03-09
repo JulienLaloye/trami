6342de622fd373a63f988bfb714d8413f6b01d9a
bhawana srivastava <bhawana.sri2021@gmail.com>
2024-03-09 14:36:26 +0100

    roomsindexpage swipper implementation

---
+++ app/javascript/controllers/roomcard_swiper_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+// import Swiper from 'swiper/bundle';
+
+// Connects to data-controller="roomcard-swiper"
+export default class extends Controller {
+  connect() {var swiper = new Swiper(".roomcardSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    centeredSlides: true,
+    grabCursor: true,
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  }
+}
