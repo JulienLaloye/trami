f9311e3808c7b2bc38f3f28be0018ebc8b6e954c
Thu Kieu <t.kieu@live.de>
2024-03-12 20:43:20 +0100

    correct the spacing issue in the new-room

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,7 +4,7 @@ class DashboardsController < ApplicationController
     @rooms = @user.rooms
     @appointments = @user.appointments
     @my_rooms = Room.where(user_id: @user.id)
-    @rooms = Room.limit(2)
-    # @rooms = Room.all
+    @rooms = Room.limit(5)
+    @reviews = Review.where(user_id: @user.id)
   end
 end
