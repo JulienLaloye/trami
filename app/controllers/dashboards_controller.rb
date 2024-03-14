6798c35f58d655857aa7a223633d2dfbe6b7faa5
Thu Kieu <t.kieu@live.de>
2024-03-14 20:32:08 +0100

    fix the swiper

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,7 +4,6 @@ class DashboardsController < ApplicationController
     @rooms = @user.rooms
     @appointments = @user.appointments
     @my_rooms = Room.where(user_id: @user.id)
-    @rooms = Room.limit(5)
     @reviews = Review.where(user_id: @user.id)
   end
 
