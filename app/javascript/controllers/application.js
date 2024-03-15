de75769fba51d5ffc422a2bdfe0d2338ad724eb1
Sarida Pinyoying <jrysrd@gmail.com>
2024-03-15 22:23:16 +0100

    not so ready questionaires page need help with next-btn, explore-btn double values in city and backend coding filtering for index page

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,6 +2,14 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
+application.registerActionOption("open", ({ event }) => {
+  if (event.type == "toggle") {
+    return event.target.open == true
+  } else {
+    return true
+  }
+})
+
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
