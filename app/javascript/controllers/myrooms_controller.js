b5b1c670fc091556263aaedc4604f8bacb9fed73
Thu Kieu <t.kieu@live.de>
2024-03-16 12:43:06 +0100

    finish the my-rooms-swiper

---
+++ app/javascript/controllers/myrooms_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  connect() {
+    console.log("2nd Stimulus here!");
+    var swiper = new Swiper(".myroomsSwiper", {
+      slidesPerView: 3,
+      spaceBetween: 40,
+      centeredSlides: false,
+      grabCursor: false,
+      loop: false,
+
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }
+    });
+  }
+}
