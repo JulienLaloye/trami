9c1c4fedf38be3c9a769e0b1bbaff4c273750833
Julien Laloye <julienlaloye@outlook.fr>
2024-03-23 13:58:20 +0100

    fixing dashboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,7 @@ class DashboardsController < ApplicationController
   def show
     @user = current_user
     @rooms = @user.rooms.where.not(user: @user)
+    @rooms = @rooms.select { |room| room.date >= Date.today }
     @appointments = @user.appointments
     @my_rooms = Room.where(user: @user)
     @reviews = Review.where(user: @user)
@@ -14,6 +15,7 @@ class DashboardsController < ApplicationController
   def update
     @user = current_user
     @my_rooms = Room.where(user: @user.id)
+    @my_rooms = @my_rooms.select { |room| room.date >= Date.today }
     @my_rooms.update(my_rooms_params)
     redirect_to dashboard_path
   end
